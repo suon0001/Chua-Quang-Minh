0c19351129151cbf71a1713d13532bf06efd95a1
suon0001 <56873514+suon0001@users.noreply.github.com>
2023-05-09 17:10:45 +0200

    save

--- view/details.php
+++ view/details.php
@@ -1,1 +1,17 @@
-<img src="assets/event/sakura_wallpaper.jpg" alt="">
+<?php
+require("rootPath.php");
+
+require $rootPath . "model/DetailsModel.php";
+require $rootPath . "controller/DetailsController.php";
+
+
+?>
+
+<?php
+
+while ($row = $eventResult->fetch(PDO::FETCH_ASSOC)) {
+    ?>
+
+    <img class="text-center" src="assets/event/<?php echo $row['pdf_file'] ?>" alt="">
+
+<?php } ?>
